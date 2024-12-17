75799dd5573fdb17f05ec3eab0d0d212a9257e06
Cyprien Ferreira <cyferrei@paul-f4Br6s2.clusters.42paris.fr>
2024-12-17 15:49:07 +0100

    [MODIF]: modif main to allow only one argument

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 17:16:44 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/12/12 14:30:21 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/12/17 12:24:49 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int argc, char **argv) {
 	(void)argc;
 	
-	if (!argv[1] || argv[1] == NULL)
+	if (!argv[1] || argv[1] == NULL || argc > 2)
 		return (-1);
 	ScalarConverter::convert(argv[1]);
 	return 0;
